13c21967c151237a99a5a5856dfb224e962101a7
BuchiProf <profbuchi@gmail.comm>
2023-11-25 10:36:52 +0100

    mise à jour vers opencv 4.8.x : changement le l'objet arcuo avec de nouvelles méthodes

--- 0_creation_detecteur.py
+++ 0_creation_detecteur.py
@@ -3,10 +3,26 @@ import numpy as np
  
 
 #création d'un dictionnaire de marqueurs ArUco.
-dico = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
+dico = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
  
-# initialisation d'un détecteur de marqueurs
-parametres =  cv.aruco.DetectorParameters_create()
- 
-# détection des marqueurs dans un cadre
-coord_coins_marqueurs, mid_marqueurs, candidats_rejetes = cv.aruco.detectMarkers(fenetre, dico, parameters=parametres)
+# initialisation des paramètres d'un détecteur de marqueurs
+parametres =  cv.aruco.DetectorParameters()
+
+#création du détecteur
+detecteur = cv.aruco.ArucoDetector(dico, parametres)
+
+# charger une image avec 'IMREAD'
+img = cv.imread("6markers_test.png")
+
+# détection des markers avec renvoie des coins, des ids et des rejets
+coord_coins_marker, id_marker, candidats_rejetes = detecteur.detectMarkers(img)
+
+#la fonction qui dessine les contours
+cv.aruco.drawDetectedMarkers(img, coord_coins_marker, id_marker,(0, 0, 255))
+
+# on affiche le resultat
+cv.imshow("6markers_test.png", img)
+
+#waitKey indique la durée d'affichage de l'image en ms
+#si la valeur est "0" alors elle ne ferme pas
+cv.waitKey(0)
